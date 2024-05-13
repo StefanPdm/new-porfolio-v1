a6bca1277ee22d339dfd0239c66388c923297378
Stefan Heinemann <115097982+StefanPdm@users.noreply.github.com>
2024-05-13 23:45:00 +0200

    Home component added

--- components/Layout.js
+++ components/Layout.js
@@ -1,30 +1,33 @@
 // fonts
-import { Sora } from 'next/font/google';
+import { Sora } from "next/font/google";
 
 // font settings
 
 export const sora = Sora({
-  subsets: ['latin'],
-  variable: '--font-sora',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 });
 
 // components
-import Nav from '../components/Nav';
-import Header from '../components/Header';
-import TopLeftImage from '../components/TopLeftImg';
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import TopLeftImage from "../components/TopLeftImg";
 
 const Layout = ({ children }) => {
   return (
-    <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable}
+
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable}
     font-sora relative
-    `}>
+    `}
+    >
       <TopLeftImage />
       <Nav />
       <Header />
       {children}
     </div>
-  )
+  );
 };
 
 export default Layout;
